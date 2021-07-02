c01b5f905a6186834782e3d07086e00558b7c581
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2021-07-02 11:12:56 -0400

    Create sampleFunction.R

---
+++ R/sampleFunction.R
@@ -0,0 +1,13 @@
+  
+#' @title Yin's Version of Neural Network through Keras Framework
+#' @description This package uses Keras as framework which provides a high-level neural networks API developed with a focus on enabling fast experimentation.
+#' @param symbol
+#' @return NULL
+#' @examples
+#' @export sampleFunction
+#'
+#' # Define function
+sampleFunction <- function(a,b) {
+  y = a + b
+  return(y)
+}
