0c005f42d7aa116fe5275fc1eb60337da61c00d5
Yiqiao-Yin <eagle0504@gmail.com>
2021-07-02 11:29:45 -0400

    added square

--- R/sampleFunction.R
+++ R/sampleFunction.R
@@ -1,4 +1,4 @@
-  
+
 #' @title Yin's Version of Neural Network through Keras Framework
 #' @description This package uses Keras as framework which provides a high-level neural networks API developed with a focus on enabling fast experimentation.
 #' @param symbol
@@ -8,6 +8,6 @@
 #'
 #' # Define function
 sampleFunction <- function(a,b) {
-  y = a + b
+  y = a + b^2
   return(y)
 }
